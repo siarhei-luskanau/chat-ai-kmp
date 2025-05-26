e2dcbd6e92bed009808a66334fbd839b17468391
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-05-26 17:09:31 +0200

    Added empty gradle project and GitHub Actions workflow

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "ChatAiKmp"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+include()
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
