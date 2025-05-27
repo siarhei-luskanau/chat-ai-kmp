38f4ce8f12b1863cd0f784d48be3ff97a5e66f8f
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-05-27 04:52:29 +0200

    Added Compose Multiplatform project from Wizard. Added GitHub actions.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,17 @@
 rootProject.name = "ChatAiKmp"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-include()
+include(":composeApp")
 
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+                includeGroupByRegex("android.*")
+            }
+        }
         gradlePluginPortal()
         mavenCentral()
     }
@@ -12,7 +19,18 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+                includeGroupByRegex("android.*")
+            }
+        }
         mavenCentral()
     }
+}
+plugins {
+    // https://github.com/JetBrains/compose-hot-reload?tab=readme-ov-file#set-up-automatic-provisioning-of-the-jetbrains-runtime-jbr-via-gradle
+    id("org.gradle.toolchains.foojay-resolver-convention").version("0.10.0")
 }
